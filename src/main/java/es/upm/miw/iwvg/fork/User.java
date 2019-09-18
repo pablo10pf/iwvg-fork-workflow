83ce50dac715ebc086600a925dc0a05a65cb3a42
pablo perez <pperezj@alumnos.unex.es>
2019-09-18 12:10:42 +0200

    add C:User

---
+++ src/main/java/es/upm/miw/iwvg/fork/User.java
@@ -0,0 +1,31 @@
+package es.upm.miw.iwvg.fork;
+
+public class User {
+    private int code;
+    private String name;
+    private String fullname;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+}
