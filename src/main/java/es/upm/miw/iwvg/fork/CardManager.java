208c5366d4f13b78b3d5edc2716341155d7924e4
Luismoteando <LuisMiguel.Ortiz.Rozalen@alumnos.upm.es>
2019-09-18 12:09:31 +0200

    added CardManager constructor

--- src/main/java/es/upm/miw/iwvg/fork/CardManager.java
+++ src/main/java/es/upm/miw/iwvg/fork/CardManager.java
@@ -1,4 +1,13 @@
 package es.upm.miw.iwvg.fork;
 
+import javax.smartcardio.Card;
+import java.util.ArrayList;
+
 public class CardManager {
+
+    private ArrayList<Card> cards;
+
+    public CardManager(ArrayList<Card> cards) {
+        this.cards = cards;
+    }
 }
